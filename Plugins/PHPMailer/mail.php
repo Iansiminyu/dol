b838029c390df2354302292f194928fb6ff63f26
Iansiminyu <ian.siminyu@strathmore.edu>
2025-09-26 10:24:32 +0300

    Configured Composer and PHPMailer

---
+++ Plugins/PHPMailer/mail.php
@@ -0,0 +1,38 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader (created by composer, not included with PHPMailer)
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'ian.siminyu@starthmore.edu';                     //SMTP username
+    $mail->Password   = 'ntnw yxdr ybld tabu';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('ian.siminyu@starthmore.edu', 'Ian Siminyu');
+    $mail->addAddress('siminyuian@gmail.com', 'Mike Oxlong');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Greetings from Postgres';
+    $mail->Body    = 'Welcome to <b>Postgres</b>! We are glad to have you on board!';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
--- conf.php
+++ conf.php
@@ -2,7 +2,7 @@
 // Site configuration
 $conf['site_name'] = "BBIT DevOps";
 $conf['site_email'] = "info@bbit.edu";
-$conf['site_url'] = "http://localhost/dol";
+$conf['site_url'] = "http://localhost/dol/";
 
 // Site language
 $conf['language'] = "en";
@@ -12,4 +12,18 @@ $conf['db_type'] = "pdo";
 $conf['db_host'] = "localhost";
 $conf['db_user'] = "root";
 $conf['db_pass'] = "0000";
-$conf['db_name'] = "dbpro";
+$conf['db_name'] = "pro";
+
+// Email configuration
+$conf['mail_type'] = "smtp"; // Options: smtp, sendmail, mail
+$conf['smtp_host'] = "smtp.gmail.com";
+$conf['smtp_user'] = "ian.siminyu@starthmore.edu";
+$conf['smtp_pass'] = "ntnw yxdr ybld tabu";
+$conf['smtp_port'] = 465;
+$conf['smtp_secure'] = "ssl";
+
+// Valid email domains for registration
+$conf['valid_email_domains'] = ["gmail.com", "yahoo.com", "outlook.com", "strathmore.edu"];
+
+// Valid password length
+$conf['min_password_length'] = 4;
